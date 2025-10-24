5dbd269a20d9361ed9ba5515a1dadd1cf8ee93b0
Sofía Tello <tellosofia@outlook.com>
2025-10-24 19:15:35 +0200

    Exercise 12: Solved

--- src/main/java/com/campusdual/classroom/Car.java
+++ src/main/java/com/campusdual/classroom/Car.java
@@ -1,5 +1,8 @@
 package com.campusdual.classroom;
 
+import com.campusdual.Utils;
+import jdk.jshell.execution.Util;
+
 public class Car {
     public String brand;
     public String model;
@@ -24,65 +27,106 @@ public class Car {
     }
 
     public void start() {
-        if (this.tachometer == 0){
+        if (isTachometerEqualToZero() && this.gear == "N"){
             this.tachometer = 1000;
-            System.out.println("Vehículo encendido");
+            System.out.println("Your vehicle is on");
         }else{
-            System.out.println("Vehículo ya está encendido");
+            System.out.println("Your vehicle is already on");
         }
     }
 
     public void stop() {
-        if (this.speedometer == 0){
+        if (this.speedometer == 0 && this.gear == "N"){
             this.tachometer = 0;
-            System.out.println("Vehículo apagado");
+            System.out.println("Your car is off");
         }else{
-            System.out.println("No se puede apagar el vehículo. Primero tiene que estar detenido");
+            System.out.println("Can't turn vehicle off. It must be stopped first");
         }
     }
     public void accelerate(){
-
+        if(this.tachometer >= 1000 && this.speedometer >= 0) {
+            this.gear = "D";
+            if (this.speedometer < MAX_SPEED){
+                this.speedometer = this.speedometer + 5;
+                System.out.println("Speed: " + this.speedometer + " km/h");
+            } else {
+                System.out.println("Max. speed cannot be exceeded");
+            }
+        } else {
+            System.out.println("Turn on your vehicle");
+        }
     }
 
     public void brake(){
-
+        if (this.speedometer > 0) {
+            this.speedometer = this.speedometer - 5;
+            System.out.println("Speed: " + this.speedometer + " km/h");
+        } else {
+            System.out.println("You're stopped :)");
+        }
     }
 
-    public void turnAngleOfWheels(int angle){
+    public void turnAngleOfWheels(int angle) {
+        int total_angle = this.wheelsAngle + angle;
 
+        if (total_angle >= -45 && total_angle <= 45) {
+            this.wheelsAngle += angle;
+        } else {
+            System.out.println("You can't turn wheels further");
+            if (angle > 0) {
+                this.wheelsAngle = 45;
+            } else {
+                this.wheelsAngle = -45;
+            }
+        }
+        System.out.println("Wheels Angle: " + this.wheelsAngle);
     }
 
-    public String showSteeringWheelDetail(){
+        public String showSteeringWheelDetail () {
+            return "";
+        }
 
-        return "";
+        public boolean isReverse () {
+           return this.reverse;
+        }
+
+        public void setReverse (boolean reverse){
+            if (this.speedometer == 0 && isTachometerGreaterThanZero()) {
+                this.gear = "R";
+                this.reverse = reverse;
+            } else {
+                System.out.println("Stop your vehicle first");
+                this.reverse = false;
+            }
+        }
+
+        public void showDetails () {
+
+        }
+
+        public boolean isTachometerGreaterThanZero () {
+            if (this.tachometer > 0) {
+                System.out.println("Revolutions per minute: " + this.tachometer);
+                return true;
+            } else {
+                System.out.println("Your tachometer is at zero");
+                return false;
+            }
+        }
+
+        public boolean isTachometerEqualToZero () {
+            if (this.tachometer == 0) {
+                System.out.println("Your tachometer is at zero");
+                return true;
+            } else {
+                System.out.println("Revolutions per minute: " + this.tachometer);
+                return false;
+            }
+        }
+
+        public static void main (String[]args){
+
+        }
+
+
     }
-
-    public boolean isReverse(){
-
-        return false;
-    }
-
-    public void setReverse(boolean reverse){
-
-    }
-
-    public void showDetails(){
-
-    }
-
-    public boolean isTachometerGreaterThanZero() {
-
-        return false;
-    }
-
-    public boolean isTachometerEqualToZero() {
-
-        return false;
-    }
-
-    public static void main(String[] args) {
-
-    }
-
-
-}
--- src/main/java/com/campusdual/classroom/Exercise.java
+++ src/main/java/com/campusdual/classroom/Exercise.java
@@ -2,6 +2,57 @@ package com.campusdual.classroom;
 
 public class Exercise {
     public static void main(String[] args) {
+        // Un método booleano que verifique si el valor del tacómetro es cero
+        Car myCar = new Car();
+        System.out.println(myCar.isTachometerEqualToZero());
+
+        // Encender el coche comprobando el valor correcto del tacómetro (investigar cuál es el valor correcto)
+        myCar.start();
+
+        // Encender el coche cuando ya está encendido (comprobar que no se puede encender si ya está encendido)
+        myCar.start();
+
+        // Apagar el coche comprobando que el valor del tacómetro es correcto
+        myCar.stop();
+
+        // Apagar el coche mientras todavía está en movimiento (y comprobar que no se puede)
+        myCar.start();
+        for (int i = 0; i < 4; i++) {
+            myCar.accelerate();
+        }
+        myCar.stop();
+
+        // Acelerar el coche
+        for (int i = 0; i < 6; i++) {
+            myCar.accelerate();
+        }
+
+        // Acelerar hasta intentar superar la velocidad máxima (y comprobar que no se puede)
+        for (int i = 0; i < 10; i++) {
+            myCar.accelerate();
+        }
+
+        // Frenar la velocidad
+        myCar.brake();
+
+        // Frenar hasta un valor negativo (y comprobar que no se puede)
+        for (int i = 0; i < 20; i++) {
+            myCar.brake();
+        }
+
+        // Girar el volante 20 grados
+        myCar.turnAngleOfWheels(20);
+
+        // Girar el volante más de 45 grados ó -45 grados (y comprobar que no se puede)
+        myCar.turnAngleOfWheels(60);
+
+        // Detener el coche y poner marcha atrás
+        myCar.stop();
+
+        // Poner marcha atrás mientras el coche está en movimiento marcha adelante (y comprobar que no se puede)
+        myCar.start();
+        myCar.accelerate();
+        myCar.setReverse(true);
 
     }
 
